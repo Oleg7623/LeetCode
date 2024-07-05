7d533f974a8d556a0ab67e265cf1a5095d6e5ecc
OLEH BAZAN <117159735+Oleg7623@users.noreply.github.com>
2024-07-05 12:51:32 +0300

    Time: 90 ms (67.49%), Space: 16.6 MB (13.38%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = []
+        for i in range(9):
+            rows.append(set())
+        
+        cols = []
+        for i in range(9):
+            cols.append(set())
+        boxes = []
+        for i in range(9):
+            boxes.append(set())
+        
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+                
+                if num == '.':
+                    continue
+                
+                box_index = (r // 3) * 3 + (c // 3)
+                
+                # Перевіряємо наявність дублікатів у рядку, стовпці та 3x3 коробці
+                if num in rows[r]:
+                    return False
+                if num in cols[c]:
+                    return False
+                if num in boxes[box_index]:
+                    return False
+                
+                # Додаємо число до відповідних множин рядка, стовпця та 3x3 коробки
+                rows[r].add(num)
+                cols[c].add(num)
+                boxes[box_index].add(num)
+        
+        return True
